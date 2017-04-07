037fecd5bbfea87a6591d7dd8f3821e14e5b7093
HATIMS <hatimsaleem@hotmail.com>
2017-04-07 12:37:31 -0700

    Updating the Java SDK to be based on Android Studio

--- android/examples/weighter-simplexml/gen/com/microsoft/hsg/android/demo/weight/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.microsoft.hsg.android.demo.weight;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
